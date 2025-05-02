37e2629125fd515587cf9266bcf9fa729b6c3b85
0xA3Z <125816778+LowKernelx@users.noreply.github.com>
2025-05-02 17:19:42 +0300

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,25 @@
+#include <iostream> // libeary
+
+using namespace std; // Variable
+
+int main(){
+
+int number; // Variable
+cin >> number; // input
+
+if(number % 2 == 0){
+    cout << "number: ";
+    cout << "The number: " << number << endl <<"Is Odd\n";
+}
+else{
+    cout << "The number: " << number << endl <<"Is Even\n";
+}
+
+return 0;
+
+}
+
+
+
+
+
